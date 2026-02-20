abf72d0873b39889812fbd6b418d8347c184dd4a
Jeganathan Swaminathan <mail2jegan@gmail.com>
2026-02-20 18:10:11 +0530

    Added reactjs application

--- Day5/reactjs/src/App.js
+++ Day5/reactjs/src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import { useState } from 'react';
+import './styles.css';
 
 function App() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>🚀 React App Running on OpenShift</h1>
-      <p>Hello Jegan! This is deployed successfully.</p>
-    </div>
-  );
+	const [count, setCount] = useState(0);
+
+	return (
+		<div className="App">
+			<header>
+				<h1>Hello from React on Red Hat Openshift !</h1>
+			</header>
+			<main>
+				<h2>Simple Counter</h2>
+				<p>Count: {count}</p>
+				<button onClick={() => setCount(c => c + 1)}>Increment</button>
+				<button onClick={() => setCount(c => c - 1)}>Increment</button>
+				<button onClick={() => setCount(0)}>Reset</button>
+			</main>
+			<footer>
+				<small>Deployed with Dockerfile - Nginx</small>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
