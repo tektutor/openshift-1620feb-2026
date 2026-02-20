228e5825151cf6f0130bcb241fc6f12d556d1b10
Jeganathan Swaminathan <mail2jegan@gmail.com>
2026-02-20 10:35:30 +0530

    Added Day5 lab exercises.

---
+++ Day5/reactjs/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>🚀 React App Running on OpenShift</h1>
+      <p>Hello Jegan! This is deployed successfully.</p>
+    </div>
+  );
+}
+
+export default App;
