e8eca6099b0bf257c73233e7ac9a98355b1968b3
Jeganathan Swaminathan <mail2jegan@gmail.com>
2026-02-20 13:38:50 +0530

    Cleaned up

--- Day5/statefulset/deploy.sh
+++ Day5/statefulset/deploy.sh
@@ -1,8 +1,8 @@
 echo -n "\nDeploying mysql primary master ...."
-oc apply -f mysql-secret.yml --save-config
-oc apply -f mysql-primarymaster-service.yml --save-config
+oc apply -f mysql-secret.yml --save-configs
+oc apply -f mysql-primarymaster-service.yml --save-configs
 
 echo -n "\nDeploying mysql replica slaves ..."
-oc apply -f mysql-sfs1.yml
-oc apply -f mysql-sfs2.yml
-oc apply -f mysql-service.yml
+oc apply -f mysql-sfs1.yml --save-configs
+oc apply -f mysql-sfs2.yml --save-configs
+oc apply -f mysql-service.yml --save-configs
